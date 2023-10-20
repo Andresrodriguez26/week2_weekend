fd3b7731c2e3355b09544fd6861928c6575cd540
Andresrodriguez26 <anferome10@gmail.com>
2023-10-20 16:17:38 -0400

    Added Score

--- project_rps.py
+++ project_rps.py
@@ -16,15 +16,25 @@ def determine_winner(computer, player):
         return "You lose"
     else:
         return "You win"
+    
+player_score = 0
+computer_score = 0
 
 while True:
-    player = input("Enter your weapon or type 'I Quit' to exit the game ").title()
-    if player == ('I Quit'):
+    player_input = input("Enter your weapon or type 'I Quit' to exit the game ").title()
+    if player_input == ('I Quit'):
         print ("Thank you for playing")
         break
-    if player not in ['Rock', 'Paper', 'Scissors']:
+    if player_input not in ['Rock', 'Paper', 'Scissors']:
         print ("pick Rock Paper or Scissors")
     else: 
         computer = computer_choice()
-        result = determine_winner(player,computer)
+        result = determine_winner(computer, player_input)
         print(f'computer choice {computer}. {result} ')
+
+        if result == "You win":
+            player_score += 1
+        elif result == "You lose":
+            computer_score += 1
+    print(f"Your score: {player_score} | Computer score: {computer_score} ")
+
