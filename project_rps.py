ae31dc2ef2ac889078be3178e36dd289a6d3ea59
Andresrodriguez26 <anferome10@gmail.com>
2023-10-20 12:45:42 -0400

    added function to determine winner

--- project_rps.py
+++ project_rps.py
@@ -5,3 +5,15 @@ def computer_choice():
     choices = ['Rock', 'Paper', 'Scissors']
     return random.choice(choices)
 
+def determine_winner(computer, player):
+    if computer == player:
+        return "Game Tied"
+    elif player == "Rock" and computer == "Paper":
+        return "You lose"
+    elif player == "Scissors" and computer == "Rock":
+        return "You lose"
+    elif player == "Paper" and computer == "Scissors":
+        return "You lose"
+    else:
+        return "You win"
+    
